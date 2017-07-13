8e2b9b875552fedb6546775b96b1d3680c160d67
auroramarch <auroramb90@gmail.com>
2017-07-13 19:19:35 +0200

    all done

--- pizza.js
+++ pizza.js
@@ -1,49 +1,88 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document). on ('ready', function(){
 
-	$('.btn-pepperonni').on('click',function(){
-		$('.pep').fadeToggle();
-	});
 
-	$('.btn-mushrooms').on('click',function(){
-		$('.mushroom').fadeToggle();
-	});
+    var total = 21;
+    var li = $('.panel.price li');
 
-	$('.btn-green-peppers').on('click',function(){
-		$('.green-pepper').fadeToggle();
-	});
-
-	$('.btn-sauce').on('click',function(){
-		$('section.sauce').toggleClass('sauce-white');
-	});
-
-	
-	$('.btn-crust').on('click',function(){
-		$('section.crust').toggleClass('crust-gluten-free');
-
-	});
 
 	$('.btn-pepperonni').on('click',function(){
+		
+		$('.pep').fadeToggle(); 
+		
+		if ($(this).hasClass('active')==false){
+			total= total+1;
+		}
+		else if ($(this).hasClass('active')==true){
+			total= total-1;
+		}
+
+		$('strong').text('$'+total);
+
+		$(li[0]).fadeToggle(1000);
 		$('button.btn-pepperonni').toggleClass('active');
 	});
 
 	$('.btn-mushrooms').on('click',function(){
+		
+		$('.mushroom').fadeToggle();
+		if ($(this).hasClass('active')==false){
+			total= total+1;
+		}
+		else if ($(this).hasClass('active')==true){
+			total= total-1;
+		}
+
+		$('strong').text('$'+total);
+		$(li[1]).fadeToggle(1000);
 		$('button.btn-mushrooms').toggleClass('active');
 	});
 
 	$('.btn-green-peppers').on('click',function(){
+		
+		$('.green-pepper').fadeToggle();
+		if ($(this).hasClass('active')==false){
+			total= total+1;
+		}
+		else if ($(this).hasClass('active')==true){
+			total= total-1;
+		}
+
+		$('strong').text('$'+total);
+		$(li[2]).fadeToggle(1000);
 		$('button.btn-green-peppers').toggleClass('active');
 	});
 
 	$('.btn-sauce').on('click',function(){
+		
+		$('section.sauce').toggleClass('sauce-white');
+		if ($(this).hasClass('active')==false){
+			total= total+3;
+		}
+		else if ($(this).hasClass('active')==true){
+			total= total-3;
+		}
+
+		$('strong').text('$'+total);
+		$(li[3]).fadeToggle(1000);
 		$('button.btn-sauce').toggleClass('active');
 	});
 
+	
 	$('.btn-crust').on('click',function(){
+		
+		$('section.crust').toggleClass('crust-gluten-free');
+		if ($(this).hasClass('active')==false){
+			total= total+5;
+		}
+		else if ($(this).hasClass('active')==true){
+			total= total-5;
+		}
+
+		$('strong').text('$'+ total);
+		$(li[4]).fadeToggle(1000);
 		$('button.btn-crust').toggleClass('active');
+
 	});
-
-
-
 
 }); 
